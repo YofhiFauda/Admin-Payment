340323ed93694a8062f7a2b719f382709180fe68
YofhiFauda <yofhi132@gmail.com>
2026-05-31 14:26:42 +0700

    fix: remove pulse

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,7 +40,5 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->withProviders([
-        \App\Providers\PulseServiceProvider::class,
-    ])
+
     ->create();
